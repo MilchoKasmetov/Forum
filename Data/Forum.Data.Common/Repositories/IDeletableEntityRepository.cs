a3b10bd44251c7b534eae3a9e46bca6b61286ccb
Milcho <m.kasmetov@gmail.com>
2023-02-26 13:10:29 +0200

    Initial creation

---
+++ Data/Forum.Data.Common/Repositories/IDeletableEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Forum.Data.Common.Repositories
+{
+    using System.Linq;
+
+    using Forum.Data.Common.Models;
+
+    public interface IDeletableEntityRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IDeletableEntity
+    {
+        IQueryable<TEntity> AllWithDeleted();
+
+        IQueryable<TEntity> AllAsNoTrackingWithDeleted();
+
+        void HardDelete(TEntity entity);
+
+        void Undelete(TEntity entity);
+    }
+}
